fb434386da2a0957f3bc436efea81bb5ebeb9b97
jonasa64 <hvidovrefifa@gmail.com>
2020-01-28 14:13:13 +0100

    Routes

--- pizzaShop/routes/web.php
+++ pizzaShop/routes/web.php
@@ -16,9 +16,15 @@ Route::get('/', function () {
 });
 
 Route::get('pizza/','PizzaController@index')->name('pizza.index');
-Route::get('pizza/create', 'PizzaContoller@ create')->name('pizza.create');
-Route::get('pizza/{pizza}','PizzaContoller@show')->name('pizza.show');
+Route::get('pizza/create', 'PizzaContoller@create')->name('pizza.create');
+Route::get('pizza/{id}','PizzaController@show')->name('pizza.show');
 Route::delete('pizza/{pizza}', 'PizzaContoller@destroy')->name('pizza.destroy');
 Route::post('pizza/','PizzaContoller@store')->name('pizza.store');
 Route::put('pizza/{pizza}/edit','PizzaContoller@edit')->name('pizza.edit');
 
+Route::get('cart/', 'CartController@index')->name('cart.index');
+Route::post('cart/{id}', 'CartController@store');
+Route::delete('cart/{id}', 'CartController@destroy')->name('cart.destroy');
+
+Route::get('checkout/', 'CheckoutController@index')->name('checkout.index');
+Route::post('checkout/', 'CheckoutController@store')->name('checkout.store');
