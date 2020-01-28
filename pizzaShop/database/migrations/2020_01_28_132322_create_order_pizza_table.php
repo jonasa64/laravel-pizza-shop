08511bd699e6595923b8a6c3c50462d9daf0c740
jonasa64 <hvidovrefifa@gmail.com>
2020-01-28 14:27:09 +0100

    Pizza order tabale migration

---
+++ pizzaShop/database/migrations/2020_01_28_132322_create_order_pizza_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderPizzaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_pizza', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('order_id');
+            $table->unsignedInteger('pizza_id');
+            $table->foreign('order_id')->references('users')->on('id');
+            $table->foreign('pizza_id')->references('pizza')->on('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_pizza');
+    }
+}
